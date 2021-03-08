0be68b906e85a709e02c06f64c1e3656ab8d246c
Mohammad jaradat <morjaradat66@gmail.com>
2021-03-08 15:24:35 +0200

    new js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 function name() {
     alert ('hello hello')
 }
-// alert();
-var print = function (){
-for (var i=0 ; v<3 ; i=i+1){
+// alert(); 
+
+
+var print = function () {
+for (var i=0 ; i <= 3 ; i=i+1){
     document.write('mohammad jaradat')
 }
 }
